836b365521dc659d5f838bd7046d0d4011db331c
yukiej <yukiej@gmail.com>
2018-10-03 15:35:08 -0700

    Add actions for CHANGE_VIDEO and CHANGE_VIDEO_LIST that pass all tests!

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,9 @@
-var changeVideo = (video) => ({
+var changeVideo = (video) => {
   //TODO:  Return some action object to change the currently playing video.
-});
+  return {
+    type: 'CHANGE_VIDEO',
+    video: video
+  };
+};
 
 export default changeVideo;
--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,8 @@
-var changeVideoList = (videos) => ({
-  //TODO:  Return some action object to change the list of videos in VideoList.
-});
+var changeVideoList = (videos) => {
+  return {
+    type: 'CHANGE_VIDEO_LIST',
+    videos: videos
+  };
+};
 
 export default changeVideoList;
